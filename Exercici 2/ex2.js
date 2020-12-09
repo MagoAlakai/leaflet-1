c6a595644eb21f953544e4ef856bbd0a35408b1f
Jose Maria Farfan <magoalakai@gmail.com>
2020-12-09 12:33:19 +0100

    Exercici 2

--- Exercici 2/ex2.js
+++ Exercici 2/ex2.js
@@ -2,15 +2,22 @@
 let mymap = L.map('mapid').setView([41.386875, 2.16994], 16);
 
 //Colocar la plantilla para que se vea el mapa
-
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 maxZoom: 18
 }).addTo(mymap);
 
-//Añadir un marcador geográfico
-let marker = L.marker([41.3868561, 2.1661102]).addTo(mymap);
+//Función para añadir marcador y popup al clickar
+let marker = null;
 
-//Añadir un pop up al marcador geográfico
-marker.bindPopup("<b>Restaurant Centfocs</b><br>Restaurante Mediterráneo<br>Carrer de Balmes, 16, 08007 Barcelona").openPopup();
+let onMapClick = (e)=> {
+  if (marker !== null) {
+    mymap.removeLayer(marker);
+  }
+  marker = L.marker(e.latlng).addTo(mymap);
+  marker.bindPopup("<b>Mis coordenadas son:</b><br>Lat: " + e.latlng.lat + "Lng: " + e.latlng.lat, e.latlng.lng).openPopup();
+};
+
+//Event listener para llamar función alclickar
+mymap.addEventListener('click', onMapClick);
 
